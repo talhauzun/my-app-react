d7df932e16a89c29dfeb71df1bac0e985c5cb13c
Talha Uzun <talhauzun600@gmail.com>
2020-08-24 22:22:42 +0300

    seçilen kişiye dair bilgileri gösteriyor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,80 @@
-import React, { Component } from 'react'
+import React, { useMemo } from 'react';
+import { useTable } from 'react-final-table';
 
-export default class App extends Component {
-
-
-
-  render() {
-    return (
-      <div>
-        Talha Uzun
-      </div>
+const columns = [
+  { name: 'id', 
+    hidden: true
+  },
+  {
+    name: 'first_name',
+    label: 'First Name',
+    render: ({ value }) => (
+      <><span role="img" aria-label="mage">🧙</span>{value}</>
     )
-  }
+  },
+  {
+
+    name: 'last_name',
+    label: 'Last Name',
+  },
+];
+
+const data = [
+  {
+    id: 1,
+    first_name: 'Frodo',
+    last_name: 'Baggins',
+  },
+  {
+    id: 2,
+    first_name: 'Samwise',
+    last_name: 'Gamgee',
+  },
+];
+
+function App() {
+
+  const memoColumns = useMemo(() => columns, []);
+  const memoData = useMemo(() => data, []);
+
+  const { headers, rows, selectRow, selectedRows } = useTable(memoColumns, memoData, {
+    selectable: true,
+  });
+  
+  return (
+    <>
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          {headers.map((header, idx) => (
+            <th key={idx}>{header.label}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((row, idx) => (
+          <tr key={idx}>
+            <td>
+              <input type="checkbox" onChange={(e) => {
+                  selectRow(row.id)
+                }} 
+              />
+            </td>
+            {row.cells.map((cell, idx) => (
+              <td key={idx}>{cell.render()}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    <pre>
+      <code>
+        {JSON.stringify(selectedRows, null, 2)}
+      </code>
+    </pre>
+    </>
+  );
 }
+
+export default App;
